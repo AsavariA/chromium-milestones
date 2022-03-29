bf9540da85ed9c0618f24eefbdc9a981c80eac88
Asavari Ambavane <ambavaneasavari@gmail.com>
2022-03-29 19:18:15 +0530

    ui bits

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
-import "./sass/index.css"
+import "./sass/index.css";
 import EnhancedTable from "./components/EnhancedTable";
 
 function App() {
-
   return (
     <div className="App">
       <EnhancedTable />
