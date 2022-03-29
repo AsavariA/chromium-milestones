169780842f4d62b7b805859209f37dc34f074eeb
Asavari Ambavane <ambavaneasavari@gmail.com>
2022-03-29 17:26:04 +0530

    edit and delete

--- src/services/fire_functions.js
+++ src/services/fire_functions.js
@@ -1,10 +1,9 @@
-import { collection, addDoc, query, onSnapshot, orderBy } from "firebase/firestore";
+import { collection, updateDoc, deleteDoc,  doc, setDoc, query, onSnapshot, orderBy } from "firebase/firestore";
 import db from "./fire";
 
 export const handleCreate = async (message, result, status, handleClose) => {
-  console.log(message, result, status);
   try {
-    const docRef = await addDoc(collection(db, "jobs"), {
+    const docRef = await setDoc(doc(db, "jobs", ), {
       message: message,
       result: result,
       status: status,
@@ -23,7 +22,8 @@ export const handleRead = (setRows) => {
   onSnapshot(q, (querySnapshot) => {
     const jobs = [];
     querySnapshot.forEach((doc) => {
-      jobs.push(doc.data());
+      var obj = {...doc.data(), id: doc.id}
+      jobs.push(obj);
     });
     setRows(jobs)
   });
@@ -39,3 +39,30 @@ export const sortDocs = (setRows, property, order) => {
     setRows(jobs)
   });
 };
+
+export const handleEditDoc = async (id, message, result, status, handleClose) => {
+  try {
+    const docRef = doc(db, "jobs", id);
+    await updateDoc(docRef, {
+      message: message,
+      status: status,
+      result: result,
+      updated: new Date()
+  });
+    console.log("Document Updated Succesfully", docRef.id);
+    handleClose()
+  } catch (e) {
+    console.error("Error editing document: ", e);
+  }
+}
+
+export const handleDelete = async (id, handleClose) => {
+  try {
+    const docRef = doc(db, "jobs", id);
+    await deleteDoc(docRef);
+    console.log("Document Deleted Succesfully");
+    handleClose()
+  } catch (e) {
+    console.error("Error deleting document: ", e);
+  }
+}
