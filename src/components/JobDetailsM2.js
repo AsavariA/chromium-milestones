ea622c353d8e659870539e846cb07175301b1063
Asavari Ambavane <ambavaneasavari@gmail.com>
2022-04-12 18:09:48 +0530

    milestone 2 pagination and filter

--- src/components/JobDetailsM2.js
+++ src/components/JobDetailsM2.js
@@ -14,7 +14,7 @@ import {
 } from "@mui/material";
 import { time_since } from "../services/time_since";
 
-const JobDetailsM2 = ({ id }) => {
+const JobDetailsM2 = ({ id, filterFields }) => {
   const [aggArray, setAggArray] = useState({});
   const [aggStatus, setAggStatus] = useState("");
 
@@ -23,82 +23,85 @@ const JobDetailsM2 = ({ id }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
-    <Box
-      sx={{
-        width: "100%",
-        mt: 2,
-        padding: "1rem",
-        borderRadius: "10px",
-        backgroundColor: "#edeef0"
-      }}
-    >
-      <p>
-        Job Id: <i>{id}</i>
-      </p>
-      <br />
-      <p>
-        Aggregate Status: <i>{aggStatus}</i>
-      </p>
-      {Object.keys(aggArray).map((x, y) => {
-        return (
-          <Box key={y} sx={{ width: "100%", mt: 2, mb: 2 }}>
-            <Accordion
-            >
-              <AccordionSummary>
-                <p>{`Aggregator ${y + 1}`}</p>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Table>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Id</TableCell>
-                      <TableCell>Message</TableCell>
-                      <TableCell>Result</TableCell>
-                      <TableCell>Status</TableCell>
-                      <TableCell>Created</TableCell>
-                      <TableCell>Updated</TableCell>
-                      <TableCell align="center">Total Levels</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    <TableRow>
-                      <TableCell>{aggArray[x][0].id}</TableCell>
-                      <TableCell>{aggArray[x][0].message}</TableCell>
-                      <TableCell>{aggArray[x][0].result}</TableCell>
-                      <TableCell>{aggArray[x][0].status}</TableCell>
-                      <TableCell>
-                        {time_since(aggArray[x][0].created.toDate())}
-                      </TableCell>
-                      <TableCell>
-                        {time_since(aggArray[x][0].updated.toDate())}
-                      </TableCell>
-                      <TableCell align="center">
-                        {aggArray[x][0].total_levels}
-                      </TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>{aggArray[x][1].id}</TableCell>
-                      <TableCell>{aggArray[x][1].message}</TableCell>
-                      <TableCell>{aggArray[x][1].result}</TableCell>
-                      <TableCell>{aggArray[x][1].status}</TableCell>
-                      <TableCell>
-                        {time_since(aggArray[x][1].created.toDate())}
-                      </TableCell>
-                      <TableCell>
-                        {time_since(aggArray[x][1].updated.toDate())}
-                      </TableCell>
-                      <TableCell align="center">
-                        {aggArray[x][1].total_levels}
-                      </TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-              </AccordionDetails>
-            </Accordion>
-          </Box>
-        );
-      })}
-    </Box>
+    <div>
+      {filterFields.includes(aggStatus) ? (
+        <Box
+          sx={{
+            width: "100%",
+            mt: 2,
+            padding: "1rem",
+            borderRadius: "10px",
+            backgroundColor: "#edeef0",
+          }}
+        >
+          <p>
+            Job Id: <i>{id}</i>
+          </p>
+          <br />
+          <p>
+            Aggregate Status: <i>{aggStatus}</i>
+          </p>
+          {Object.keys(aggArray).map((x, y) => {
+            return (
+              <Box key={y} sx={{ width: "100%", mt: 2, mb: 2 }}>
+                <Accordion>
+                  <AccordionSummary>
+                    <p>{`Aggregator ${y + 1}`}</p>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Table>
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>Id</TableCell>
+                          <TableCell>Message</TableCell>
+                          <TableCell>Result</TableCell>
+                          <TableCell>Status</TableCell>
+                          <TableCell>Created</TableCell>
+                          <TableCell>Updated</TableCell>
+                          <TableCell align="center">Total Levels</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        <TableRow>
+                          <TableCell>{aggArray[x][0].id}</TableCell>
+                          <TableCell>{aggArray[x][0].message}</TableCell>
+                          <TableCell>{aggArray[x][0].result}</TableCell>
+                          <TableCell>{aggArray[x][0].status}</TableCell>
+                          <TableCell>
+                            {time_since(aggArray[x][0].created.toDate())}
+                          </TableCell>
+                          <TableCell>
+                            {time_since(aggArray[x][0].updated.toDate())}
+                          </TableCell>
+                          <TableCell align="center">
+                            {aggArray[x][0].total_levels}
+                          </TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>{aggArray[x][1].id}</TableCell>
+                          <TableCell>{aggArray[x][1].message}</TableCell>
+                          <TableCell>{aggArray[x][1].result}</TableCell>
+                          <TableCell>{aggArray[x][1].status}</TableCell>
+                          <TableCell>
+                            {time_since(aggArray[x][1].created.toDate())}
+                          </TableCell>
+                          <TableCell>
+                            {time_since(aggArray[x][1].updated.toDate())}
+                          </TableCell>
+                          <TableCell align="center">
+                            {aggArray[x][1].total_levels}
+                          </TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </AccordionDetails>
+                </Accordion>
+              </Box>
+            );
+          })}
+        </Box>
+      ) : null}
+    </div>
   );
 };
 
--- src/components/Milestone2.js
+++ src/components/Milestone2.js
@@ -1,27 +1,136 @@
-import { Grid } from "@mui/material";
+import { Grid, Pagination } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import { handleDocIdsMilestone2 } from "../services/fire_functions";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import FilterListIcon from "@mui/icons-material/FilterList";
 
 import JobDetailsM2 from "./JobDetailsM2";
 
-const Milestone2 = () => {
+const Milestone2 = (props) => {
   const [idArray, setIdArray] = useState([]);
+  const [dataPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const [filterFields, setFilterFields] = useState([
+    "Scheduled",
+    "Running",
+    "Finished",
+    "Failed",
+  ]);
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleChange = (e, statusName) => {
+    if (!e.target.checked) {
+      setFilterFields(filterFields.filter((item) => item !== statusName));
+    } else {
+      setFilterFields((filterFields) => [...filterFields, statusName]);
+    }
+  };
+
+  const indexOfLastJob = currentPage * dataPerPage;
+  const indexOfFirstJob = indexOfLastJob - dataPerPage;
+  const currentData = idArray.slice(indexOfFirstJob, indexOfLastJob);
 
   useEffect(() => {
     handleDocIdsMilestone2(setIdArray);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const handleChangePagination = (event, value) => {
+    setCurrentPage(value);
+  };
+
   return (
-    <Grid container spacing={2}>
-      {idArray.map((x) => {
-        return (
-          <Grid item xs={12} key={x} sm={12} md={6}>
-            <JobDetailsM2 id={x} />
-          </Grid>
-        );
-      })}
-    </Grid>
+    <>
+      <Tooltip title="Filter list">
+        <IconButton onClick={handleOpen}>
+          <FilterListIcon />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={filterFields.includes("Scheduled")}
+                onChange={(e) => handleChange(e, "Scheduled")}
+              />
+            }
+            label="Scheduled"
+          />
+        </MenuItem>
+        <MenuItem>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={filterFields.includes("Running")}
+                onChange={(e) => handleChange(e, "Running")}
+              />
+            }
+            label="Running"
+          />
+        </MenuItem>
+        <MenuItem>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={filterFields.includes("Finished")}
+                onChange={(e) => handleChange(e, "Finished")}
+              />
+            }
+            label="Finished"
+          />
+        </MenuItem>
+        <MenuItem>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={filterFields.includes("Failed")}
+                onChange={(e) => handleChange(e, "Failed")}
+              />
+            }
+            label="Failed"
+          />
+        </MenuItem>
+      </Menu>
+      <Grid container spacing={2}>
+        {currentData.map((x) => {
+          return (
+            <Grid item xs={12} key={x} sm={12} md={6}>
+              <JobDetailsM2 filterFields={filterFields} id={x} />
+            </Grid>
+          );
+        })}
+      </Grid>
+      <div className="pagination">
+        <Pagination
+          count={Math.ceil(idArray.length / dataPerPage)}
+          variant="outlined"
+          color="primary"
+          shape="rounded"
+          page={currentPage}
+          onChange={handleChangePagination}
+        />
+      </div>
+    </>
   );
 };
 
